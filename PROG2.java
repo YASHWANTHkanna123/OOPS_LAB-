b58fc00cdc4f9da596877cfd991df1036e2472a5
YASHWANTHkanna123 <yashwanth200804@gmail.com>
2025-12-29 12:34:05 +0530

    Add files via upload

---
+++ PROG2.java
@@ -0,0 +1,11 @@
+class PROG2 {
+    public static void main(String[] args) {
+        int a= 200;
+        int b= 100;
+        System.out.print("the sum of two numbers is ");
+        System.out.println(a+b);
+        System.out.print("the difference of two numbers is ");
+        System.out.println(a-b);
+    
+    }
+}
---
+++ PROG3.java
@@ -0,0 +1,11 @@
+class PROG3 {
+    public static void main(String[] args) {
+        int a= 12;
+        int b= 14;
+        System.out.print("the multiplication of two numbers is");
+        System.out.println(a*b);
+        System.out.print("the division of two numbers is ");
+        System.out.println(a/b);
+
+    }
+}
